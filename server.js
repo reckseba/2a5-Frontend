261cab4422698b3424d681f4a9ff111a9174ce5b
Sebastian Reck <mail@s-reck.de>
2020-12-31 16:52:00 +0100

    added security handler for redirect on http attempt

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ const credentials = {
 	ca: ca
 };
 
+// Add a handler to inspect the req.secure flag (see 
+// http://expressjs.com/api#req.secure). This allows us 
+// to know whether the request was via http or https.
+app.use (function (req, res, next) {
+	if (req.secure) {
+			// request was via https, so do no special handling
+			next();
+	} else {
+			// request was via http, so redirect to https
+			res.redirect('https://' + req.headers.host + req.url);
+	}
+});
+
 const filter = function (pathname, req) {
 	const regex = /^\/[A-Za-z0-9]{3,5}/gm;
 	return pathname.match(regex) && pathname.length >= (3+1) && pathname.length <= (5+1) && req.method === 'GET';
