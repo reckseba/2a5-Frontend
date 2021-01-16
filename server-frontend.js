05da9293fc0b4d58bf26dc0711891a14774c70ed
Sebastian Reck <mail@s-reck.de>
2021-01-16 19:01:08 +0100

    csp go live

--- server-frontend.js
+++ server-frontend.js
@@ -28,7 +28,7 @@ app.use (function (req, res, next) {
 			// set csp
 			
 			res.setHeader(
-				'Content-Security-Policy-Report-Only',
+				'Content-Security-Policy',
 				"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self' 'sha256-AAoB7NZRA776ALHTNBNJ0JbtXU4iafu0oepkvAsAGCE='; style-src 'self'; frame-src 'self'"
 			);
 			
